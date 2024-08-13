7ebdb2fc1f7f3490185c828df9c3bba245f04237
Fede Rudolf <federicorudolf@hotmail.com>
2024-08-12 21:04:07 -0300

    Changed framework

--- src/Services/main.service.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getBitcoinPrice = () => {
-  const url = "https://us-central1-myprofile-4928d.cloudfunctions.net/getBitcoinPrice";
-  return axios.get(url);
-}
